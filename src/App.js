7c8cef0e72c0106135322cd74b58c6bc16e6eda4
RAHUL YADAV <rahulyadav@RAHULs-MacBook-Air.local>
2024-01-31 13:41:15 +0530

    project intialized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="text-3xl font-bold underline text-green-500">
+          Hello world!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
